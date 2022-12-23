4abfc2fb921a3bd4e52ea01a3932000a1e68ff56
Metin Acir <metinacir9@gmail.com>
2022-12-23 14:11:26 +0300

    Encapsulation.Car is created

--- src/Day38_OOP_Inheritance_Overriding/car_Task/Car.java
+++ src/Day38_OOP_Inheritance_Overriding/car_Task/Car.java
@@ -46,6 +46,8 @@ carTask:
                 '}';
     }
 
+
+
     /*
     public void fly(){
 
---
+++ src/Day38_OOP_Inheritance_Overriding/car_Task/Toyota.java
@@ -0,0 +1,13 @@
+package src.Day38_OOP_Inheritance_Overriding.car_Task;
+
+public class Toyota extends Car {
+
+
+    public Toyota( String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
+    }
+    public void reliable(){
+        System.out.println(brand+" "+model+ " is reliable");
+    }
+
+}
