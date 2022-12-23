b6adfaebc0717713ab58280becf77c4d52c8f1e7
Metin Acir <metinacir9@gmail.com>
2022-12-23 14:14:36 +0300

    Encapsulation.Car is created

---
+++ src/Day38_OOP_Inheritance_Overriding/car_Task/BMW.java
@@ -0,0 +1,7 @@
+package src.Day38_OOP_Inheritance_Overriding.car_Task;
+
+public class BMW extends Car{
+    public BMW( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+}
---
+++ src/Day38_OOP_Inheritance_Overriding/car_Task/Tesla.java
@@ -0,0 +1,7 @@
+package src.Day38_OOP_Inheritance_Overriding.car_Task;
+
+public class Tesla extends Car{
+    public Tesla( String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+}
